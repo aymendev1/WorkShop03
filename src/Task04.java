7b54dfb9f5e584a9dad185c3c4f8c8ff053d5918
Ayman Azougar <topayman5@gmail.com>
2022-03-12 13:58:21 +0100

    Fixing the TASK NO4

--- src/Task04.java
+++ src/Task04.java
@@ -3,10 +3,13 @@ import java.io.IOException;
 public class Task04 {
     public static void main(String[] args) throws IOException {
         System.out.println("Even numbers from 1 to 100 are :");
+        int result=0;
         for (int i = 0; i <= 100; i++) {
             if(i%2==0){
-                System.out.println(i);
+                result +=i;
+
             }
         }
+        System.out.println(result);
     }
 }
